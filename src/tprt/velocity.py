cf7d0972a9b1e0907207c4b5bf1b7ca8347ea1d3
Serafim <s.grubas@g.nsu.ru>
2018-05-02 00:37:20 +0700

    Добавил метод get_dv

--- src/tprt/velocity.py
+++ src/tprt/velocity.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
 LAYER_KIND = {
     'iso': _iso_model,
@@ -9,12 +10,20 @@ LAYER_KIND = {
 class Velocity:
     def get_velocity(self, x):
         pass
+    def get_dv(self, x):
+        pass
 
 
 class ISOVelocity(Velocity):
     def __init__(self, vp, vs):
         self.vp = vp
         self.vs = vs
+        self.dvp = np.array([0,0])
+        self.dvs = np.array([0,0])
+
 
     def get_velocity(self, x):
         return {'vp': self.vp, 'vs': self.vs}
+
+    def get_dv(self, x):
+        return {'vp': self.dvp, 'vs': self.dvs}
