b8bb22a3cb8392af1cedc31254b97c3eb9912605
Serafim <s.grubas@g.nsu.ru>
2018-10-13 11:48:44 +0700

    Убрано разделение на отдельные слои. Первому и последнему слою добавлены горизонты с глубиной + и - infinity

--- src/tprt/velocity_model.py
+++ src/tprt/velocity_model.py
@@ -7,10 +7,7 @@ from src.tprt import Layer, FlatHorizon
 class Velocity_model(object):
     def __init__(self, velocity, density, name, horizons):
         self.horizons = horizons
-        self.top_layer = None
-        self.mid_layers = []
-        self.bottom_layer = None
-        self.split_layers(self.make_layers(velocity, density, name))
+        self.layers = self.make_layers(velocity, density, name)
 
     @staticmethod
     def make_flat_horizons(depths, anchors, dips, azimuths):        # Подразумеватся что все отсортировано по глубине от 0 до ...
@@ -22,21 +19,14 @@ class Velocity_model(object):
     def make_layers(self, velocities, densities, names):            # Подразумеавется, что все подано в нужной последовательности
         Layers = []
         for i, (velocity, density, name) in enumerate(zip(velocities, densities, names)):
-            if i!=0: top = self.horizons[i-1]
-            else: top = None
-            if i<len(self.horizons): bottom = self.horizons[i]
-            else: bottom = None
+
+            if i==0: top = FlatHorizon(depth=-np.inf)
+            else: top = self.horizons[i-1]
+
+            if i<len(velocities)-1: bottom = self.horizons[i]
+            else: bottom = FlatHorizon(depth=np.inf)
             Layers.append(Layer(velocity, density, top, bottom, name=name))
         return Layers
-
-    def split_layers(self, all_layers):
-        self.top_layer = all_layers[0]
-        self.mid_layers = all_layers[1:-1]
-        if len(all_layers) == 1:
-            self.bottom_layer = None
-        else:
-            self.bottom_layer = all_layers[-1]
-
 
     def add_horizon(self, Horizon):
         self.horizons.append(Horizon)
