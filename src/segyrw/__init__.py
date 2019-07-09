ea4e94eb89b46c8c9f13bebe522fe39199bbb000
LoginovGN <loginovgeorgy@gmail.com>
2019-07-09 10:25:48 +0700

    update seislet and moment tensor

--- src/segyrw/__init__.py
+++ src/segyrw/__init__.py
@@ -1,3 +1,4 @@
 from .read import read_trace_header, read_bin_header, read_textual_header, read_traces
 from .header import *
+from .utils import *
 
--- src/smti/decomposition.py
+++ src/smti/decomposition.py
@@ -1,14 +1,22 @@
 import numpy as np
 
 
-def _alternative_decomposition(m1, m2, m3):
-    """
+def projection(m):
+    (_, sigma, _) = np.linalg.svd(m)
+    sigma = np.sort(sigma)[-1::-1]
+    m1, m2, m3 = sigma
 
-    :param m1:
-    :param m2:
-    :param m3:
-    :return:
-    """
+    scale = 0.5 * (np.abs(m1 + m2 + m3) + m1 - m3)
+
+    k0 = .5 / scale
+    k_iso = k0 * (m1 + m2 + m3)
+    k_clvd = k0 * (m1 - 2 * m2 + m3)
+    k_dc = k0 * (m1 - m2 - np.abs(m1 + m3 - 2 * m2))
+
+    return k_iso, k_clvd, k_dc
+
+
+def _alternative_decomposition(m1, m2, m3):
     scale = 0.5 * (np.abs(m1 + m2 + m3) + m1 - m3)
     k0 = 1 / (2 * scale)
     k_iso = k0 * (m1 + m2 + m3)
@@ -26,16 +34,46 @@ def _basic_decomposition(m1, m2, m3):
     return k_iso, k_clvd, k_dc
 
 
-def decomposition(m, dec_type='Alternative'):
+def decomposition(m, basic=True):
     (sigma, _) = np.linalg.eig(m)
 
     sigma = np.sort(sigma)[-1::-1]
     sigma = sigma / np.max(np.abs(sigma))
     m1, m2, m3 = sigma
 
-    k_iso, k_clvd, k_dc = _alternative_decomposition(m1, m2, m3)
+    if basic:
+        k_iso, k_clvd, k_dc = _basic_decomposition(m1, m2, m3)
+    else:
+        k_iso, k_clvd, k_dc = _alternative_decomposition(m1, m2, m3)
 
     return k_iso, k_clvd, k_dc
+
+
+def decompose_m(m):
+    # by Vavricuk
+
+    eig_val, eig_vector = np.linalg.eig(m)
+
+    volumetric = eig_val.mean()
+    deviatoric = np.eye(3)*eig_val - volumetric*np.eye(3)
+
+    value_dev, _ = np.linalg.eig(deviatoric)
+    abs_values = np.abs(value_dev)
+    sort_idx = abs_values.argsort()[::-1]
+    value_dev = value_dev[sort_idx]
+
+    maximum_value = value_dev[0]
+    minimum_value = value_dev[2]
+
+    max_value_m = np.max(np.abs(eig_val))
+
+    iso = 100 * volumetric / max_value_m
+    epsilon = - minimum_value / maximum_value
+
+    clvd = 2 * np.sign(maximum_value) * epsilon * (100 - np.abs(iso))
+    dc = 100 - np.abs(iso) - np.abs(clvd)
+
+    return iso, dc, clvd
 
 
 def decomposition_vu(m):
@@ -55,18 +93,3 @@ def decomposition_vu(m):
     v = (1 / 3) * (m1 + m2 + m3)
 
     return v, u
-
-
-def projection(M):
-    (_, sigma, _) = np.linalg.svd(M)
-    sigma = np.sort(sigma)[-1::-1]
-    m1, m2, m3 = sigma
-
-    scale = 0.5 * (np.abs(m1 + m2 + m3) + m1 - m3)
-
-    k0 = .5 / scale
-    k_iso = k0 * (m1 + m2 + m3)
-    k_clvd = k0 * (m1 - 2 * m2 + m3)
-    k_dc = k0 * (m1 - m2 - np.abs(m1 + m3 - 2 * m2))
-
-    return k_iso, k_clvd, k_dc
