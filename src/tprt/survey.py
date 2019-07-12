38a7b401957cc7cf94b41bf601a0e321e687ede4
Serafim <s.grubas@g.nsu.ru>
2019-07-12 13:56:05 +0700

    prettifying and adding plot method

--- src/tprt/survey.py
+++ src/tprt/survey.py
@@ -4,43 +4,51 @@ from .ray import Ray
 class Survey:
     def __init__(self, sources, receivers, vel_mod):
         """
-        :param sources:     list of objects of type of Source
-        :param receivers:   list of objects of type of Receiver
-        :param vel_mod:     object of type of Velocity_model
+        :param sources:     array of objects of type of Source
+        :param receivers:   array of objects of type of Receiver
+        :param vel_mod:     object of type of VelocityModel
         """
-        self.sources = sources
-        self.receivers = receivers
+        self.sources = np.array(sources, ndmin=1)
+        self.receivers = np.array(receivers, ndmin=1)
         self.velmod = vel_mod
 
-        self.rays = []
+        self.rays = None
         self.traveltimes = None
 
     def initialize_rays(self, reflect_horizon=2, vtype='vp', forward=False):
         """
-
         :param reflect_horizon: number of reflect horizon with respect to source layer
         :param forward: Is it forward wave? (boolean)
         :param vtype: type of wave ('vs', 'vp')
         :return: update property "rays"
         """
-
-        for sou in self.sources:
-            for rec in self.receivers:
-                raycode_ij = None
-                if not forward:
-                    raycode_ij = Ray.get_raycode(sou, rec, reflect_horizon, self.velmod, vtype)
-                self.rays.append(Ray(sou, rec, self.velmod, raycode=raycode_ij, vtype=vtype))
-
+        self.rays = np.empty(shape=self.sources.shape+self.receivers.shape, dtype=Ray)
+        for i, sou in np.ndenumerate(self.sources):
+            for j, rec in np.ndenumerate(self.receivers):
+                raycode_ij = Ray.get_raycode(sou, rec, reflect_horizon, self.velmod, vtype, forward=forward)
+                self.rays[i+j] = Ray(sou, rec, self.velmod, raycode=raycode_ij, vtype=vtype)
+        
     def calculate(self, method='BFGS', survey2D=False):
-        for ray_i in self.rays:
-            ray_i.optimize(method=method, survey2D=survey2D)
+        self.traveltimes = np.empty(shape=self.rays.shape, dtype=float)
+        for i, ray in np.ndenumerate(self.rays):
+            ray.optimize(method=method, survey2D=survey2D)
+            ray.ray_amplitude = ray.compute_ray_amplitude()[0]
+            self.traveltimes[i] = ray.traveltime
+            
 
-    def plot(self, ax):
-        for rec in self.receivers:
+    def plot(self, **kwargs):
+        if not np.any(kwargs.get('ax')):
+            fig = plt.figure()
+            ax = Axes3D(fig)
+        else:
+            ax = kwargs['ax']
+            kwargs.pop('ax')
+        
+        for i, rec in np.ndenumerate(self.receivers):
             rec.plot(ax=ax)
-        for ray in self.rays:
+        for i, ray in np.ndenumerate(self.rays):
             ray.plot(ax=ax)
-        for sou in self.sources:
+        for i, sou in np.ndenumerate(self.sources):
             sou.plot(ax=ax, marker='^', color='k')
 
     def get_traveltimes(self):
